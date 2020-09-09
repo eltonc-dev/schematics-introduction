b71278f1e22814498c98978752f13d5dabe2c2e7
Elton Coelho <eltonc.dev@gmail.com>
2020-09-09 00:45:46 -0300

    explanation comments

--- src/hello-template/index.ts
+++ src/hello-template/index.ts
@@ -2,16 +2,35 @@ import { Rule, SchematicContext, Tree, url, apply, mergeWith, template } from '@
 import { strings } from '@angular-devkit/core';
 
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function helloTemplate(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+
+    /**
+     * Utilizo a funcão url para acessar o contexto de um diretório
+     * nesse caso, pego o contexto do diretório files, dentro do diretório atual
+     */
     const templateFile = url('./files');
+
+    /**
+     * Utilizo a funcão apply para aplicar ao contexto informado (templateFile), 
+     * as rules que vão fazer as modificar o contexto passado
+     * Nesse caso, só é passada uma unica rule já existente (template), 
+     * para injetar no contexto informado algumas informações (options, strings)
+     * options = opções informadas no CLI e definidas no schema.json
+     * string = conjunto de funções oferecidas pelo pacote core...que ofere funcões como `classify`, `dasherize` (utilizada para criar o nome do arquivo = `__name@dasherize__`)
+     * 
+     */
     const resolvedTemplate = apply(templateFile, [
       template({..._options,
         ...strings
       })
     ]);
+
+    /**
+     * Retorno o resultado da função mergeWith
+     * A função mergeWith, é responsável por pegar o contexto criado (resolvedTemplate)
+     * e mesclar com o contexto atual, atribuindo as moficações na tree
+     */
     return mergeWith(resolvedTemplate)(tree, _context);
   };
 }
