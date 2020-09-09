710aa3a28d24b220bbdcee224d9e3c4f33da03f3
Elton Coelho <eltonc.dev@gmail.com>
2020-09-09 00:19:09 -0300

    create file using option name

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -2,6 +2,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export function hello(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create('hello.txt', `Hello ${_options.name}`);
     return tree;
   };
 }
